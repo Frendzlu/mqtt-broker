f93390874cd7f3f3e70d53cd05956ab8065a5963
kodziarz <gregor47@poczta.fm>
2026-01-15 00:08:05 +0100

    circular dependency removed

--- dp_mqtt/MQTTPacketFactory.py
+++ dp_mqtt/MQTTPacketFactory.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Optional
 from .protocol import GenericPacket, PacketType
-from .broker import ClientConnection
 
 class MQTTPacketFactory(ABC):
     """Abstract factory for creating MQTT packet instances."""
